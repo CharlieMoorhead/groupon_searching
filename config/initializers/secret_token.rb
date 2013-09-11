505a8d7200ecdf7f21bfc0391e3966b2831e21f2
Charlie Moorhead <Charlie@Charlies-MacBook-Air.local>
2013-09-11 18:31:10 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GrouponSearching::Application.config.secret_key_base = '578f3b3de83c848576571e92b31493ed8e13fe7e4e64cd6d4a0700eb305478e510095806557cc44fcaa37603ead904fe247f98734f702ccfb41c7510bc8f78ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GrouponSearching::Application.config.session_store :cookie_store, key: '_groupon_searching_session'
